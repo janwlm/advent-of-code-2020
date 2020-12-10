35dee9f39d15552d821c8bcb7acc47772b8bf19c
unknown <willemen.jannes@hotmail.com>
2020-12-10 11:31:05 +0100

    Solve day 10, part 1 & 2

--- Day_10/Part_1.py
+++ Day_10/Part_1.py
@@ -1,3 +1,22 @@
 def readInput():
     inputLines = open("input.txt", "r").readlines()
-    
+    tempArr = []
+    tempArr.append(0)
+    for line in inputLines:
+        tempArr.append(int(line))
+    tempArr = sorted(tempArr)
+    tempArr.append(tempArr[-1] + 3)
+    return tempArr
+
+sortedInput = readInput()
+
+difference1 = 0
+difference3 = 0
+for i in range(len(sortedInput) - 1):
+    joltDiff = sortedInput[i + 1] - sortedInput[i]
+    if(joltDiff == 1):
+        difference1 += 1
+    elif(joltDiff == 3):
+        difference3 += 1
+
+print(difference1 * difference3)
---
+++ Day_10/Part_2.py
@@ -0,0 +1,27 @@
+def readInput():
+    inputLines = open("input.txt", "r").readlines()
+    tempArr = []
+    tempArr.append(0)
+    for line in inputLines:
+        tempArr.append(int(line))
+    tempArr = sorted(tempArr)
+    tempArr.append(tempArr[-1] + 3)
+    return tempArr
+
+cache = [0]*(len(readInput()))
+def possibleSolutions(sortedInput, i):
+    if(i == (len(sortedInput) - 1)):
+        return 1
+
+    result = 0
+    for j in range(i + 1, min(i + 4, len(sortedInput))):
+        if(sortedInput[j] - sortedInput[i] <= 3 and cache[j] == 0):
+            cache[j] = possibleSolutions(sortedInput, j)
+            result += cache[j]
+        elif(sortedInput[j] - sortedInput[i] <= 3 and cache[j] != 0):
+            result += cache[j]
+
+    return result
+
+sortedInput = readInput()
+print(possibleSolutions(sortedInput, 0))
